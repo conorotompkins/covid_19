16b89b359fb9cac39c13202341fabd9c9c41abd8
Conor Tompkins <conorotompkins@gmail.com>
2020-06-29 08:43:55 -0400

    updated code to include when it was updated, added "4th" stage color

--- scripts/allegheny_county_timeline.R
+++ scripts/allegheny_county_timeline.R
@@ -3,7 +3,7 @@ library(lubridate)
 library(tidyquant)
 library(hrbrthemes)
 
-theme_set(theme_ipsum(base_size = 15, axis_title_size = 15))
+theme_set(theme_ipsum(base_size = 15, strip_text_size = 15, axis_title_size = 15))
 
 df <- read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv") %>% 
   mutate(county = str_c(county, "County", sep = " ")) %>% 
@@ -36,19 +36,23 @@ df %>%
   ggplot(aes(date, cases_new)) +
   geom_line()
 
+last_updated <- last(df$date)
+
 allegheny_county_timeline <- df %>% 
   ggplot(aes(date, cases_new_rolling_14)) +
   annotate(geom = "rect", xmin = ymd("2020-03-23"), xmax = ymd("2020-05-15"), ymin = as.Date(-Inf), ymax = as.Date(Inf), 
            fill = "red", alpha = .3) +
   annotate(geom = "rect", xmin = ymd("2020-05-15"), xmax = ymd("2020-06-05"), ymin = as.Date(-Inf), ymax = as.Date(Inf), 
            fill = "yellow", alpha = .3) +
-  annotate(geom = "rect", xmin = ymd("2020-06-05"), xmax = as.Date(Inf), ymin = as.Date(-Inf), ymax = as.Date(Inf), 
+  annotate(geom = "rect", xmin = ymd("2020-06-05"), xmax = ymd("2020-06-28"), ymin = as.Date(-Inf), ymax = as.Date(Inf), 
            fill = "green", alpha = .3) +
-  geom_point(aes(y = cases_new), alpha = .3) +
+  annotate(geom = "rect", xmin = ymd("2020-06-28"), xmax = as.Date(Inf), ymin = as.Date(-Inf), ymax = as.Date(Inf),
+           fill = "#aaff00", alpha = .3) +
+  #geom_vline(xintercept = last_updated, linetype = 2) +
+  geom_point(aes(y = cases_new), alpha = .6) +
   geom_line(size = 1.5) +
-  geom_vline(xintercept = ymd("2020-06-05"), linetype = 2) +
-  facet_wrap(~str_c(state, county, sep = ", ")) +
-  labs(title = "COVID-19 response timeline",
+  facet_wrap(~str_c(county, state, sep = ", ")) +
+  labs(title = str_c("COVID-19 response timeline (last updated ", last_updated, ")"),
        x = NULL,
        y = "New cases",
        subtitle = "Black line indicates 14-day rolling average",
@@ -57,4 +61,4 @@ allegheny_county_timeline <- df %>%
 allegheny_county_timeline
 
 allegheny_county_timeline %>% 
-  ggsave(filename = "output/allegheny_county_timeline.png", width = 12, height = 10)
+  ggsave(filename = str_c("output/ac_timeline/allegheny_county_timeline_", last_updated, ".png"), width = 12, height = 10)
