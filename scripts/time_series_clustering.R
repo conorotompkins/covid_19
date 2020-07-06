6d24ed10116075dcc94ce5e3e2cf4e44a6657df8
Conor Tompkins <conorotompkins@gmail.com>
2020-07-06 18:23:13 -0400

    started script to cluster cases timelines

---
+++ scripts/time_series_clustering.R
@@ -0,0 +1,11 @@
+#sources
+https://rpubs.com/esobolewska/dtw-time-series
+http://www.rdatamining.com/examples/time-series-clustering-classification
+http://rstudio-pubs-static.s3.amazonaws.com/398402_abe1a0343a4e4e03977de8f3791e96bb.html
+
+#package
+library(tidyverse)
+library(tsibble)
+library(dtwclust)
+
+set.seed(1234)
