daec63f3d8c678ecec846c301e27a720835e20d3
Conor Tompkins <conorotompkins@gmail.com>
2020-07-14 10:43:10 -0400

    smoothed out data with loess, added code for working with changepoint library

--- scripts/waves/changepoint_analysis.R
+++ scripts/waves/changepoint_analysis.R
@@ -62,64 +62,83 @@ df_change <- df %>%
          metric == "cases") %>% 
   filter(!is.na(value_new_rolling_14)) %>% 
   group_by(state, county, metric) %>% 
-  mutate(pct_change = (value_new_rolling_14 / lag(value_new_rolling_14)) - 1,
-         #pct_change = round(pct_change, 2)
-         ) %>% 
+  # mutate(pct_change = (value_new_rolling_14 / lag(value_new_rolling_14)) - 1,
+  #        d2 = (pct_change / lag(pct_change)) - 1
+  #        ) %>% 
+  mutate(d1 = value_new_rolling_14 - lag(value_new_rolling_14),
+         d2 = (d1 - lag(d1)) - 1) %>% 
   ungroup()
 
 
 df_change %>% 
-  ggplot(aes(date, pct_change, color = county)) +
-  geom_point() +
-  geom_smooth(span = .1) +
-  facet_wrap(~county, ncol= 1, scales = "free_y")
+  ggplot(aes(date, value_new_rolling_14, color = county)) +
+  geom_line()
 
-#smooth pct change with loess
-pct_change_model <- function(df) {
-  loess(pct_change ~ date_numeric, data = df, span = .66)
+df_change %>% 
+  ggplot(aes(date, d1, color = county)) +
+  geom_smooth(se = FALSE)# +
+  #facet_wrap(~county, ncol = 1, scales = "free_y")
+
+df_change %>% 
+  ggplot(aes(date, d2, color = county)) +
+  geom_smooth(se = FALSE)# +
+  
+df_change %>% 
+  ggplot(aes(d1, d2, color = county)) +
+  geom_point()
+  
+  #smooth pct change with loess
+d2_model <- function(df) {
+  loess(d2 ~ date_numeric, data = df, span = .66)
 }
 
-df_change <- df_change %>% 
-  filter(!is.na(pct_change),
+#df_change_nested <- 
+df_change %>% 
+  filter(!is.na(d2),
          #county != "Allegheny County"
          ) %>%
-  select(state, county, date, pct_change) %>% 
+  select(state, county, date, value_new_rolling_14, d2) %>% 
+  drop_na() %>% 
   group_by(state, county) %>% 
   mutate(date_numeric = row_number()) %>% 
   ungroup() %>% 
   group_by(state, county) %>% 
   nest() %>% 
-  mutate(model = map(data, pct_change_model),
-         fitted_data = map(model, augment)) %>% 
-  unnest(fitted_data) %>% 
-  select(-c(data, model)) %>% 
-  rename(pct_change_fitted = .fitted)
+  mutate(model = map(data, d2_model),
+         fitted_data = map(model, augment))
+
+df_change <- df_change_nested %>% 
+  unnest(c(data, fitted_data), names_repair = "universal") %>% 
+  select(-contains("..."), -model) %>% 
+  rename(d2_fitted = .fitted)
+
+
 
 df_change %>% 
   #unnest_longer(data) %>% 
   #janitor::clean_names()
-  ggplot(aes(date_numeric, .fitted)) +
+  ggplot(aes(date, d2_fitted)) +
   geom_line() +
   facet_wrap(~county)
 
   
 df_change <- df_change %>% 
-  select(state, county, date_numeric, pct_change_fitted) %>% 
-  mutate(sign = pct_change_fitted > 0,
-         change_point = sign != lag(sign, 1) & sign != lag(sign, 3) & sign != lag(sign, 5))
+  select(state, county, date, value_new_rolling_14, d2_fitted) %>% 
+  mutate(sign = d2_fitted > 0,
+         change_point = sign != lag(sign, 1))
 
 df_change_points <- df_change %>% 
   filter(change_point == TRUE) %>% 
-  select(state, county, date_numeric)
+  select(state, county, date)
 
 
 df_change %>% 
-  #pivot_longer(cols = c(value_new_rolling_14, pct_change_rolling_14)) %>%
-  ggplot(aes(date_numeric, pct_change_fitted)) +
+  pivot_longer(cols = c(value_new_rolling_14, d2_fitted)) %>%
+  ggplot(aes(date, value)) +
   geom_line() +
   #geom_point(aes(color = change_point_flag)) +
-  geom_vline(data = df_change_points, aes(xintercept = date_numeric), linetype = 2, color = "red") +
-  facet_grid(~county, scales = "free_y")
+  geom_vline(data = df_change_points, aes(xintercept = date), linetype = 2, color = "red") +
+  facet_grid(name~county, scales = "free_y")
 
 
 
@@ -133,59 +152,44 @@ df_change %>%
 
 
 
-####old code
+#changepoint library
+library(changepoint)
+
+changepoints <- df %>% 
+  filter(county == "Allegheny County",
+         metric == "cases") %>% 
+  select(value_new_rolling_14) %>% 
+  drop_na(value_new_rolling_14) %>% 
+  pull(value_new_rolling_14) %>% 
+  changepoint::cpt.var(class = FALSE, method = "PELT", 
+                       penalty='Manual', 
+                       pen.value='2*log(n)',
+                       minseglen = 10) %>% 
+  enframe()
+
+changepoints
 
 df %>% 
-  filter(state == "Pennsylvania",
-         county == "Allegheny County",
-         metric == "cases") %>% 
-  filter(!is.na(value_new_rolling_14)) %>% 
+  filter(county == "Allegheny County",
+         metric == "cases") %>%  
   mutate(index = row_number()) %>% 
   ggplot(aes(index, value_new_rolling_14)) +
   geom_point() +
-  geom_vline(xintercept = 98)
-  
-df_change <- df %>% 
-  filter(state == "Pennsylvania",
-         county == "Allegheny County",
-         metric == "cases") %>% 
-  filter(!is.na(value_new_rolling_14)) %>% 
-  group_by(id, metric) %>% 
-  mutate(pct_change = deriv(value_new_rolling_14))
-  #mutate(pct_change = (value_new_rolling_14 / lag(value_new_rolling_14)) / 100) %>% 
-  ungroup() %>% 
-  group_by(id, metric) %>% 
-  tq_mutate(
-    # tq_mutate args
-    select     = pct_change,
-    mutate_fun = rollapply, 
-    # rollapply args
-    width      = 14,
-    align      = "right",
-    FUN        = mean,
-    # mean args
-    na.rm      = TRUE,
-    # tq_mutate args
-    col_rename = "pct_change_rolling_14"
-  ) %>% 
-  ungroup() %>% 
-  filter(!is.na(pct_change_rolling_14))
+  geom_vline(data = changepoints, aes(xintercept = value))
 
-df_change <- df_change %>% 
-  group_by(id, metric) %>% 
-  mutate(change_in_change = pct_change_rolling_14 - lag(pct_change_rolling_14)) %>% 
-  ungroup()
 
-df_change %>% 
-  mutate(index = row_number()) %>% 
-  ggplot(aes(index, pct_change_rolling_14)) +
-  geom_point() +
-  geom_smooth() +
-  geom_vline(xintercept = 95)
 
-changepoint::cpt.mean(df_change$pct_change_rolling_14, class = FALSE)
+df %>% 
+  filter(metric == "cases",
+         !is.na(value_new_rolling_14)) %>% 
+  add_count(state, county) %>% 
+  filter(n > 4) %>% 
+  group_by(state, county) %>% 
+  mutate(changepoints = map_dbl(.x = value_new_rolling_14, ~cpt.var(data = ., 
+                                                               class = FALSE, 
+                                                               method = "PELT", 
+                                                               penalty='Manual',
+                                                               pen.value='2*log(n)',
+                                                               minseglen = 10)))
 
-x=c(rnorm(50,0,1),rnorm(50,5,1),rnorm(50,10,1),rnorm(50,3,1))
 
-plot(x)
-cpt.mean(x,penalty="Manual",pen.value="2*log(n)",method="BinSeg",Q=5,class=FALSE) 
