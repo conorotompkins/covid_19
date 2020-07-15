09cd145d1efdf012b7ff9ed2e6b1ac22b32e2ade
Conor Tompkins <conorotompkins@gmail.com>
2020-07-15 10:23:30 -0400

    created notes for bad data

---
+++ scripts/ac_timeline/bad_data_notes.R
@@ -0,0 +1,18 @@
+#replace deaths on July 7th with NA
+#The deaths reported today are from the state’s use of the Electronic Data Reporting System (EDRS) and include #deaths from April 5 – June 13, all decedents were 65 or older.
+#https://twitter.com/HealthAllegheny/status/1280517051589722117?s=20
+
+#replace deaths on June 4th with NA
+# This is the COVID-19 Daily Update or June 4, 2020. 
+# The data reflected in these updates include information reported to the department
+# in the past 24 hours as well as data since March 14 when the first case was reported in the county.
+#https://twitter.com/HealthAllegheny/status/1268558422242463748?s=20
+
+
+#The data reflected in these updates include info reported in the past 24 hours
+#as well as data since March 14 when the first case was reported in the county.
+#https://twitter.com/HealthAllegheny/status/1260587935289704449?s=20
+
+#https://twitter.com/HealthAllegheny/status/1250801169989074944?s=20
+
+#https://twitter.com/HealthAllegheny/status/1275805982522855424?s=20
